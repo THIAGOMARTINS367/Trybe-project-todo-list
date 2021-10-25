047af70ee3f079e9a56839c131d6a40f028e3113
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-10-25 20:06:48 -0300

    Requisitos 7 e 8 feitos.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createTask() {
   if (textoTarefa === '') {
     
   } else {
-    listaTarefas.innerHTML += "<li class='selected'>" + textoTarefa.value + "</li>";
+    listaTarefas.innerHTML += "<li class='li'>" + textoTarefa.value + "</li>";
 
     textoTarefa.value = '';
     
@@ -14,4 +14,46 @@ function createTask() {
 
 }
 
+function selectItem(event) {
+  let classSelected = document.querySelectorAll('.li');
+  let classListLi = event.target.classList;
+
+  for (const iterator of classListLi) {
+    if (iterator === 'selected') {
+      
+    } else {
+      event.target.classList.add('selected');
+
+    }
+    
+  }
+
+  classListLi = event.target.classList;
+  for (const key of classSelected) {
+    key.style.backgroundColor = 'white';
+
+  }
+
+  for (const iterator of classListLi) {
+    if (iterator === 'selected') {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      
+    };
+
+  }
+  
+}
+
 buttonCriarTarefa.addEventListener('click', createTask);
+
+document.addEventListener('click', function(event) {
+  let classLI = event.target.classList;
+  for (const iterator of classLI) {
+    if (iterator === 'li') {
+      selectItem(event)
+
+    };
+
+  }
+  
+});
